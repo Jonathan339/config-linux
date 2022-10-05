754baaab9562c9a21e80de952d6e2aa994b2e30f
Jonathan David Dominguez <dominguez.jonathan.david@gmail.com>
2022-10-05 18:14:30 -0300

    agregue android studio

--- install.sh
+++ install.sh
@@ -8,19 +8,13 @@ cp -r .vim ~/
 
 # Update repos
 sudo apt update
-sudo apt install libstdc++6 curl wget vlc apt-transport-https gnupg2 -y
+sudo apt install libstdc++6 curl wget vlc apt-transport-https  gnupg2 seahorse -y
 clear
 #jdk java
 sudo apt install openjdk-11-jre -y
-
-#tess termimal
-#curl -s https://apt.tessapp.dev/key.gpg | gpg --dearmor | tee /usr/share/keyrings/tess.gpg > /dev/null
-#echo 'deb [signed-by=/usr/share/keyrings/tess.gpg] https://apt.tessapp.dev stable main' | sudo tee /etc/apt/sources.list.d/tess-packages.list
-#sudo apt update && sudo apt install tess -y
 clear
 #vim plug
 git clone https://github.com/junegunn/vim-plug.git ~/.vim/plugged/vim-plug
-
 # Google Chrome
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
@@ -33,13 +27,13 @@ sudo apt-get install -y nodejs
 clear
 #Nvim
 sudo snap install nvim --classic
-
+clear
+sudo snap install android-studio --classic
 clear
 #Yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update && sudo apt install yarn -y
-
 
 #limpia despues de instalar
 sudo apt autoclean -y && sudo apt autoremove -y && sudo apt update && sudo apt upgrade -y $s_null
