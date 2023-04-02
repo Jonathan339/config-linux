e4d673166f3c21e0b843ba3668011c0c619af952
Jonathan D. Dominguez <30633652+Jonathan339@users.noreply.github.com>
2023-04-02 17:41:21 -0300

    alacritty snap

--- install.sh
+++ install.sh
@@ -18,9 +18,10 @@ packages=(
 
 # Función para instalar paquetes
 install_packages() {
-  echo "Instalando paquetes necesarios..."
+  echo -e "\e[34mInstalando paquetes necesarios...\e[0m"
   sudo apt update
   sudo apt install "${packages[@]}" -y
+  check_error "Ocurrió un error al instalar paquetes"
 }
 
 # Función para verificar si un paquete está instalado
@@ -31,34 +32,54 @@ package_is_installed() {
 # Función para instalar un paquete si no está instalado
 install_package_if_not_installed() {
   if ! package_is_installed "$1"; then
-    echo "$1 no está instalado. Instalando..."
+    echo -e "\e[34m$1 no está instalado. Instalando...\e[0m"
     sudo apt-get install "$1" -y
-    check_error
+    check_error "Ocurrió un error al instalar el paquete $1"
   else
-    echo "$1 ya está instalado. Saltando..."
+    echo -e "\e[32m$1 ya está instalado. Saltando...\e[0m"
   fi
 }
 
 # Verificación de errores después de cada comando
 check_error() {
   if [ $? -ne 0 ]; then
-    echo "Ocurrió un error en el último comando. Abortando..."
+    echo -e "\e[31m$1\e[0m"
     exit 1
   fi
 }
 
 # Copia de archivos de configuración
-echo "Copiando archivos de configuración..."
-cp -r .vimrc ~/
-check_error
+echo -e "\e[34mCopiando archivos de configuración...\e[0m"
+#cp -r .vimrc ~/
+#check_error
 cp -r .zshrc ~/
-check_error
+check_error "Ocurrió un error al copiar el archivo .zshrc"
 cp -r .bash_aliases ~/
-check_error
+check_error "Oc
+# Alacritty
+echo -e "\e[34mInstalando Alacritty...\e[0m"
+sudo snap install alacritty --classic
+check_error "Ocurrió un error al instalar Alacritty"
+
+# Android Studio
+echo -e "\e[34mInstalando Android Studio...\e[0m"
+sudo snap install android-studio --classic
+check_error "Ocurrió un error al instalar Android Studio"
+
+# Limpieza
+echo -e "\e[34mLimpieza...\e"
+sudo apt autoclean -y
+sudo apt autoremove -y
+sudo apt update
+sudo apt upgrade -y
+check_error "Ocurrió un error al limpiar"
+
+echo -e "\033[1;32mInstalación completada.\033[0m"urrió un error al copiar el archivo .bash_aliases"
 cp -r nvim ~/.config/
-check_error
-cp -r .vim ~/
-check_error
+check_error "Ocurrió un error al copiar el directorio nvim a ~/.config/"
+mkdir -p ~/.config/alacritty
+cp -r alacritty.yml ~/.config/alacritty/
+check_error "Ocurrió un error al copiar el archivo alacritty.yml"
 
 # Instalación de paquetes
 install_packages
@@ -69,36 +90,36 @@ install_package_if_not_installed google-chrome-stable
 install_package_if_not_installed nodejs
 install_package_if_not_installed yarn
 
-# Vim Plug
-echo "Instalando Vim Plug..."
-git clone https://github.com/junegunn/vim-plug.git ~/.vim/plugged/vim-plug
-check_error
-
 # Pynvim
-echo "Instalando Pynvim..."
+echo -e "\e[34mInstalando Pynvim...\e[0m"
 git clone https://github.com/neovim/pynvim.git
 cd pynvim && pip3 install .
-check_error
+check_error "Ocurrió un error al instalar Pynvim"
 cd ..
 sudo rm -rf pynvim
-check_error
+check_error "Ocurrió un error al borrar el directorio pynvim"
 
 # Nvim
-echo "Instalando Neovim..."
+echo -e "\e[34mInstalando Neovim...\e[0m"
 sudo snap install nvim --classic
-check_error
+check_error "Ocurrió un error al instalar Neovim"
+
+# Alacritty
+echo -e "\e[34mInstalando Alacritty...\e[0m"
+sudo snap install alacritty --classic
+check_error "Ocurrió un error al instalar Alacritty"
 
 # Android Studio
-echo "Instalando Android Studio..."
+echo -e "\e[34mInstalando Android Studio...\e[0m"
 sudo snap install android-studio --classic
-check_error
+check_error "Ocurrió un error al instalar Android Studio"
 
 # Limpieza
-echo "Limpieza..."
+echo -e "\e[34mLimpieza...\e"
 sudo apt autoclean -y
 sudo apt autoremove -y
 sudo apt update
 sudo apt upgrade -y
-check_error
+check_error "Ocurrió un error al limpiar"
 
-echo "Instalación completada."
+echo -e "\033[1;32mInstalación completada.\033[0m"
