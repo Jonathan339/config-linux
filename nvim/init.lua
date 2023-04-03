493d7cce244faeebe5783709a69a235c11b61388
Jonathan David Dominguez <dominguez.jonathan.david@gmail.com>
2023-04-02 21:32:30 -0300

    edite nvim/init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,10 @@
 require("keymap-config")
 require("nvim-notify-config")
+require("nvim-compe-config")
 require("options")
 require("packer-config")
 require("telescope-config")
-require("nvim-compe-config")
+
 
 
 
