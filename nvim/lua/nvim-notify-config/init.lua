af7e8a93592778ae77b5332fe7c680497b584cac
Jonathan David Dominguez <dominguez.jonathan.david@gmail.com>
2023-04-02 21:06:22 -0300

    configuración de notify

--- nvim/lua/nvim-notify-config/init.lua
+++ nvim/lua/nvim-notify-config/init.lua
@@ -1,6 +1,22 @@
-require("notify").setup {
-    display = "NvimNotify",
-    stages = 'slide',
-    background_colour = 'Normal',
-    timeout = 5000,
-  } 
+require('notify').setup({
+  -- Cambiar el método de visualización a "floating"
+  display = { 
+    floating = { 
+      border = {'┌', '─', '┐', '│', '┘', '─', '└', '│'},
+      focusable = false,
+      winblend = 10,
+    },
+  },
+  -- Personalizar la apariencia de las notificaciones
+  stages = "fade",
+  timeout = 3000,
+  -- Usar diferentes configuraciones para diferentes tipos de notificaciones
+  errors = {background_colour = "#ff0000", icon = " "},
+  warnings = {background_colour = "#ff8800", icon = " "},
+  -- Agregar acciones personalizadas a la notificación
+  actions = {
+    ["Open file"] = function(notification)
+      vim.cmd(string.format(":e %s", notification.metadata.filename))
+    end
+  }
+})
