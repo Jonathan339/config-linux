5ef0dc7a0d538d3fa55dfb9d02c8211a66331247
Jonathan David Dominguez <dominguez.jonathan.david@gmail.com>
2023-04-07 21:29:11 -0300

    atajos de teclado para coc

--- nvim/lua/keymap-config/init.lua
+++ nvim/lua/keymap-config/init.lua
@@ -32,11 +32,13 @@ vim.api.nvim_set_keymap('n', '<Leader>m', ':NERDTree<CR>', {noremap = true})
 -- Ejecutar código con SnipRun
 vim.api.nvim_set_keymap('n', '<Leader>n', ':SnipRun<CR>', {noremap = true})
 
--- Habilitar el autocompletado inteligente
-vim.api.nvim_set_keymap('i', '<Tab>', 'pumvisible() ? "<C-n>" : "<Tab>"', {expr = true})
-vim.api.nvim_set_keymap('i', '<S-Tab>', 'pumvisible() ? "<C-p>" : "<S-Tab>"', {expr = true})
-
 -- Asignaciones de teclas para pickers de Telescope
 vim.api.nvim_set_keymap('n', '<leader>ff', "<cmd>lua require('telescope.builtin').find_files()<CR>", {noremap = true})
 vim.api.nvim_set_keymap('n', '<leader>fg', "<cmd>lua require('telescope.builtin').live_grep()<CR>", {noremap = true})
 vim.api.nvim_set_keymap('n', '<leader>fb', "<cmd>lua require('telescope.builtin').buffers()<CR>", {noremap = true})
+
+-- Mapear las teclas de navegación para el autocompletado
+vim.api.nvim_set_keymap('i', '<C-n>', 'coc#refresh()<CR>', {silent = true, expr = true})
+vim.api.nvim_set_keymap('i', '<C-p>', 'coc#refresh()<CR>', {silent = true, expr = true})
+vim.api.nvim_set_keymap('i', '<Tab>', 'pumvisible() ? "<C-n>" : "<Tab>"', {silent = true, expr = true})
+vim.api.nvim_set_keymap('i', '<S-Tab>', 'pumvisible() ? "<C-p>" : "<C-h>"', {silent = true, expr = true})
