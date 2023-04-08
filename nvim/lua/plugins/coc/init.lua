8fed80cabc8ede70a36778817bb538aaec6475a6
Jonathan David Dominguez <dominguez.jonathan.david@gmail.com>
2023-04-07 22:35:54 -0300

    agregue configuracion en coc

--- nvim/lua/plugins/coc/init.lua
+++ nvim/lua/plugins/coc/init.lua
@@ -16,3 +16,9 @@ vim.g.coc_global_extensions = {
   'coc-sql',
   'coc-yaml',
 }
+
+-- Configuración para ignorar el mensaje de 'undefined global' en Lua
+-- Reemplaza 'undefinedVariable' con el nombre específico del mensaje que deseas ignorar
+-- en tu caso.
+vim.g.coc_diagnostic_ignore_sources = { 'lua.undefinedVariable' }
+
