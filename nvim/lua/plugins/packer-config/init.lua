ba0e48a4518fb9a4708348fabf2049a85013883b
Jonathan David Dominguez <dominguez.jonathan.david@gmail.com>
2023-04-22 17:34:47 -0300

    configuracion para Nerdtree

--- nvim/lua/plugins/packer-config/init.lua
+++ nvim/lua/plugins/packer-config/init.lua
@@ -1,18 +1,19 @@
 -- Instala automáticamente Packer si aún no está instalado
-local packer_install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local packer_install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 if vim.fn.empty(vim.fn.glob(packer_install_path)) > 0 then
-  vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', packer_install_path})
-  vim.cmd [[packadd packer.nvim]]
+    vim.fn.system({
+        'git', 'clone', '--depth', '1',
+        'https://github.com/wbthomason/packer.nvim', packer_install_path
+    })
+    vim.cmd [[packadd packer.nvim]]
 end
 
 -- Asegura que Packer esté instalado y configura los complementos
 local packer_ok, packer = pcall(require, "packer")
-if not packer_ok then
-  return
-end
+if not packer_ok then return end
 
 -- Autocomandos para Packer
-vim.cmd[[
+vim.cmd [[
   augroup Packer_aug
     autocmd!
     autocmd BufWritePost plugins.lua PackerCompile
@@ -22,66 +23,65 @@ vim.cmd[[
 ]]
 -- Función para limpiar, instalar y compilar plugins con Packer
 function packer_cleanup_install_compile()
-  vim.cmd([[PackerClean]])
-  vim.cmd([[PackerInstall]])
-  vim.cmd([[PackerCompile]])
+    vim.cmd([[PackerClean]])
+    vim.cmd([[PackerInstall]])
+    vim.cmd([[PackerCompile]])
 end
 
 -- Configuración de plugins
 require('packer').startup(function()
-  -- Plugin manager
-  use 'wbthomason/packer.nvim'
+    -- Plugin manager
+    use 'wbthomason/packer.nvim'
 
-  -- Tema de color
-  use 'gruvbox-community/gruvbox'
+    -- Tema de color
+    use 'gruvbox-community/gruvbox'
 
-  -- Navegación de archivos
-  use 'christoomey/vim-tmux-navigator'
-
-
-use {
-  "nvim-neo-tree/neo-tree.nvim",
-    branch = "v2.x",
-    requires = { 
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
+    use {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v2.x",
+        requires = {
+            "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim"
+        }
     }
-  }
-  -- Búsqueda y selección de archivos
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
-  use 'nvim-lua/plenary.nvim'
-  -- Git gutter
-  use 'airblade/vim-gitgutter'
+    -- Búsqueda y selección de archivos
+    use {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.1',
+        requires = {{'nvim-lua/plenary.nvim'}}
+    }
+    use 'nvim-lua/plenary.nvim'
+    -- Git gutter
+    use 'airblade/vim-gitgutter'
 
-  -- Auto-save
-  use 'Pocco81/auto-save.nvim'
-  --
-  --use 'lewis6991/gitsigns.nvim'
+    -- Auto-save
+    use 'Pocco81/auto-save.nvim'
+    --
+    use 'jmcantrell/vim-virtualenv'
 
-  -- Prettier
-  use {'prettier/vim-prettier', run = 'yarn install' }
+    -- Prettier
+    use {'prettier/vim-prettier', run = 'yarn install'}
 
-  -- Configuración de LSP
-  use 'neovim/nvim-lspconfig'
+    -- Configuración de LSP
+    use 'neovim/nvim-lspconfig'
 
-  -- Análisis sintáctico de árbol
-  use 'nvim-treesitter/nvim-treesitter'
-  use 'nvim-tree/nvim-web-devicons'
+    -- Análisis sintáctico de árbol
+    use 'nvim-treesitter/nvim-treesitter'
+    use 'nvim-tree/nvim-web-devicons'
 
- 
-  -- Notificaciones
-  use 'rcarriga/nvim-notify'
-  use {
-    'yamatsum/nvim-nonicons',
-    requires = {'kyazdani42/nvim-web-devicons'}
-  }
-  -- Integración con React
-  use 'pangloss/vim-javascript'
-  use 'yuezk/vim-js'
+    use {'junegunn/fzf', dir = '~/.fzf', run = './install --all'}
+    use 'junegunn/fzf.vim' -- necesario para las vistas previas
+    use 'scrooloose/nerdtree'
+    use 'christoomey/vim-tmux-navigator'
+    
+    -- Notificaciones
+    use 'rcarriga/nvim-notify'
+    use {'yamatsum/nvim-nonicons', requires = {'kyazdani42/nvim-web-devicons'}}
+    
+    -- Integración con React
+    use 'peitalin/vim-jsx-typescript'
+    use 'pangloss/vim-javascript'
+    use 'yuezk/vim-js'
 end)
 
 -- Configuración para pangloss/vim-javascript
@@ -96,4 +96,10 @@ vim.g.javascript_conceal_function = "ƒ"
 vim.g.javascript_conceal_null = "ø"
 vim.g.javascript_conceal_this = "@"
 
+-- configuracion para Nerdtree
+vim.g.NERDTreeShowHidden = 1
+vim.cmd[[
+  autocmd StdinReadPre * let s:std_in=1
+  autocmd VimEnter * if argc() == 0 && !exists("s:std_in") | NERDTree | endif
+]]
 
