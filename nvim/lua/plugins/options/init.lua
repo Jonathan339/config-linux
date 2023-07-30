5b8097950b114e27988af1b9f8a51d3008377596
Jonathan David Dominguez <dominguez.jonathan.david@gmail.com>
2023-07-30 00:58:54 -0300

    cambie las opciones de auto completado con tab

--- nvim/lua/plugins/options/init.lua
+++ nvim/lua/plugins/options/init.lua
@@ -1,90 +1,81 @@
--- Habilitar el soporte para sintaxis de lenguaje
+-- Configuración de codificación y manejo de archivos
+vim.cmd("autocmd!")
+vim.scriptencoding = 'utf-8'
+vim.opt.encoding = 'utf-8'
+vim.opt.fileencoding = 'utf-8'
+vim.opt.shell = 'zsh'
+vim.opt.backupskip = { '/tmp/*', '/private/tmp/*' }
+
+-- Configuración de la interfaz
+vim.opt.title = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.hlsearch = true
+vim.opt.showcmd = true
+vim.opt.cmdheight = 1
+vim.opt.laststatus = 2
+vim.opt.number = true
+vim.opt.hidden = true
+vim.opt.splitright = true
+vim.opt.wrap = false
+vim.opt.backspace = { 'start', 'eol', 'indent' }
+vim.opt.path:append { '**' }
+vim.opt.wildignore:append { '*/node_modules/*' }
+vim.opt.termguicolors = true
+vim.opt.background = "dark"
+
+-- Configuración de pestañas y espacios
+vim.opt.expandtab = true
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+
+-- Configuración de búsqueda
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.inccommand = 'split'
+
+-- Configuración adicional
 vim.o.syntax = 'on'
+vim.opt.cursorline = false
+vim.opt.guicursor = "n-v-c:block-Cursor/lCursor-blinkwait100-blinkon0-blinkoff0"
+vim.opt.showmatch = true
+vim.opt.mouse = "a"
+vim.opt.clipboard = "unnamedplus"
+vim.opt.backup = false
+vim.opt.errorbells = false
+vim.opt.swapfile = false
+vim.opt.signcolumn = "yes"
+vim.opt.buftype = ""
+vim.opt.wildmenu = true
+vim.opt.winblend = 0
+vim.opt.completeopt = 'menu'
+vim.opt.wildoptions = 'pum'
+vim.opt.pumblend = 5
 
--- Ocultar archivos abiertos al salir de Vim
-vim.o.hidden = true
-
--- Dividir la ventana en la dirección adecuada al abrir una nueva ventana
-vim.o.splitright = true
-
--- Resaltar la línea actual
-vim.o.cursorline = false
-vim.o.guicursor = "n-v-c:block-Cursor/lCursor-blinkwait100-blinkon0-blinkoff0"
-
--- Mostrar los números de línea
-vim.o.number = true
-
--- Reemplazar las pestañas con espacios
-vim.o.expandtab = true
-
--- Establecer el tamaño de las pestañas
-vim.o.softtabstop = 2
-
--- Establecer el ancho de las pestañas
-vim.o.shiftwidth = 2
-
--- Habilitar la autoindentación inteligente
-vim.o.smartindent = true
-
--- Deshabilitar el ajuste automático de líneas
-vim.o.wrap = false
-
--- Resaltar el paréntesis correspondiente
-vim.o.showmatch = true
-
--- Establecer la búsqueda sin distinción entre mayúsculas y minúsculas
-vim.o.ignorecase = true
-
--- Establecer la búsqueda con distinción entre mayúsculas y minúsculas si no todas las letras son minúsculas
-vim.o.smartcase = true
-
--- Visualización de reemplazo en vivo
-vim.o.inccommand = "split"
-
--- Habilitar el uso del mouse en Vim
-vim.o.mouse = "a"
-
--- Usar el portapapeles del sistema operativo
-vim.o.clipboard = "unnamedplus"
-
--- Deshabilitar la creación de archivos de copia de seguridad
-vim.o.backup = false
-
--- Deshabilitar las campanas de error
-vim.o.errorbells = false
-
--- Deshabilitar la creación de archivos de intercambio
-vim.o.swapfile = false
-
--- Mostrar la columna de signos siempre
-vim.o.signcolumn = "yes"
-
--- No utilizar un tipo de buffer específico
-vim.o.buftype = ""
-
--- Habilitar el autocompletado de archivos (wildmenu)
-vim.o.wildmenu = true
-
--- Configuración de opciones de complementación
+-- Configuración de autocompletado
 vim.g.completion_enable_snippet = 'vim-vsnip'
 vim.g.completion_matching_strategy_list = {'exact', 'substring', 'fuzzy'}
 vim.g.completion_trigger_character = {'.'}
 
--- Configuración adicional
--- Configuración del tema de color
+-- Configuración de tema de color
 vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
-vim.o.termguicolors = true
-vim.o.background = "dark"
 vim.cmd('colorscheme gruvbox')
 vim.o.runtimepath = vim.o.runtimepath .. ',~/.local/share/nvim/site/'
 
-
+-- Virtualenv para Python
 vim.g.virtualenv_python = 'python3'  -- Cambiar a la ruta correcta de tu ejecutable de Python
 vim.cmd([[autocmd FileType python autocmd BufReadPost,BufNewFile * VirtualEnvActivate]])
 
+-- Resaltar el texto copiado durante 200 ms usando el grupo de resaltado "Visual"
+vim.cmd [[
+  augroup highlight_yank
+  autocmd!
+  au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Visual", timeout=200})
+  augroup END
+]]
 
+-- Función de recarga de configuración
 function ReloadConfig()
     vim.cmd("source ~/.config/nvim/init.vim")
     print("Configuración recargada.")
 end
-
--- nvim/lua/plugins/settings.lua
+++ nvim/lua/plugins/settings.lua
@@ -1,11 +1,14 @@
-require("plugins.prettier.init")
-require("plugins.packer-config.init")
+require("plugins.gitgutter.init")
 require("plugins.keymap-config.init")
 require("plugins.lsp.init")
-require("plugins.options.init")
-require("plugins.telescope.init")
-require("plugins.telescope-file-browser.init")
 require("plugins.lualine.init")
 require("plugins.notifity.init")
-require("plugins.gitgutter.init")
+require("plugins.options.init")
+require("plugins.packer-config.init")
+require("plugins.prettier.init")
+require("plugins.telescope.init")
+require("plugins.telescope-file-browser.init")
+
+
+
 
