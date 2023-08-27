7c33827be64c9b83a4d9c297604e07a745f2670a
Jonathan David Dominguez <dominguez.jonathan.david@gmail.com>
2023-08-27 13:28:57 -0300

    feat(nvim): Agregue shortcuts para telescope

--- nvim/lua/jonathan339/map.lua
+++ nvim/lua/jonathan339/map.lua
@@ -13,7 +13,15 @@ vim.api.nvim_set_keymap('n', '<Leader>q', ':q!<cr>', {noremap = true})
 vim.api.nvim_set_keymap('n', '<Leader>i', ':PackerInstall<cr>', {noremap = true})
 -- Buscar archivos con Files
 vim.api.nvim_set_keymap('n', '<Leader>f', ':Telescope find_files<cr>', {noremap = true})
+-- Buscar buffers con Buffers
+vim.api.nvim_set_keymap('n', '<Leader>b', ':Telescope buffers<cr>', {noremap = true})
+-- ayuda con Telescope
+vim.api.nvim_set_keymap('n', '<Leader>h', ':Telescope help_tags<cr>', {noremap = true})
+-- busca gist con Telescope
+vim.api.nvim_set_keymap('n', '<Leader>g', ':Telescope git_files<cr>', {noremap = true})
+
 -- Abrir buffers con Buffers
 vim.api.nvim_set_keymap('n', '<Leader>b', ':Buffers<cr>', {noremap = true})
 -- terminal con ToggleTerm
 vim.api.nvim_set_keymap('n', '<Leader>t', ':ToggleTerm direction=float<cr>', {noremap = true})
+
