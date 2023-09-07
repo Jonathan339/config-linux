a674931740a0232c95b3f971189047a91b65e0d2
Jonathan David Dominguez <dominguez.jonathan.david@gmail.com>
2023-09-07 07:35:10 -0300

    feat(nvim):Actualice la configuracion de lsp

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,70 +1,63 @@
-local lsp = require("lsp-zero")
+local lsp_status, lsp = pcall(require, "lsp-zero")
+if not lsp_status then return end
 
--- Cargar una configuración predefinida para el LSP
-lsp.preset("recommended")
-
--- Asegurarse de que los siguientes servidores LSP estén instalados
-lsp.ensure_installed({
-  'tsserver',
-  'eslint',
-  'pyright',
-  'jsonls',
-  'vimls',
-  'bashls',
-  'lua_ls',
-})
-
--- Solucionar 'Undefined global vim'
-lsp.nvim_workspace()
-
-local cmp = require('cmp')
-local cmp_select = { behavior = cmp.SelectBehavior.Select }
-local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
-})
-
-cmp_mappings['<Tab>'] = nil
-cmp_mappings['<S-Tab>'] = nil
-
-lsp.setup_nvim_cmp({
-  mapping = cmp_mappings
-})
-
--- Configuración de preferencias para el LSP
-lsp.set_preferences({
-    suggest_lsp_servers = false,
-    sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
-    }
-})
-
--- Función que se ejecuta cuando se conecta un cliente LSP
 lsp.on_attach(function(client, bufnr)
-  local opts = { buffer = bufnr, remap = false }
-
-  -- Mapeos de teclas para acciones LSP
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+  lsp.default_keymaps({ buffer = bufnr })
 end)
-
--- Inicialización del LSP
-lsp.setup()
-
--- Configuración de diagnósticos visuales
-vim.diagnostic.config({
-    virtual_text = true
+-- Configura el formateo automático al guardar para ciertos servidores LSP y lenguajes.
+lsp.format_on_save({
+  enable = true,
+  ormat_opts = {
+    async = true,
+    timeout_ms = 10000,
+  },
+  servers = {
+    ["null-ls"] = { "javascript", "typescript", "lua" },
+  },
 })
+
+lsp.set_server_config({
+  single_file_support = false,
+  capabilities = {
+    textDocument = {
+      foldingRange = {
+        dynamicRegistration = false,
+        lineFoldingOnly = true
+      }
+    }
+  }
+})
+
+-- Instala servidores LSP y complementos.
+lsp.ensure_installed({
+  "eslint_lsp",
+  "rust_analyzer",
+  "sumneko_lua",
+  "lua_ls",
+  "prettier",
+  "tsserver",
+  "eslint",
+  "bashls",
+  "vimls",
+  "lua_format",
+  "jsonls",
+})
+
+-- Configura los íconos para los errores, advertencias, sugerencias e información del LSP.
+lsp.set_sign_icons({
+  error = "✘",
+  warn = "▲",
+  hint = "⚑",
+  info = "»",
+})
+-- Configura la función de autocompletado del LSP.
+lsp.omnifunc.setup({
+  autocomplete = true,
+  use_fallback = true,
+  update_on_delete = true,
+  trigger = "<C-Space>",
+})
+
+require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+
+lsp.setup()
