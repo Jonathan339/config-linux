9587f10239ca029c434ef8954e830fce3479206b
Jonathan David Dominguez <dominguez.jonathan.david@gmail.com>
2023-09-14 17:03:34 -0300

    feat(nvim): Cambie cmp.lua

--- nvim/after/plugin/cmd.lua
+++ nvim/after/plugin/cmd.lua
@@ -1,38 +1,35 @@
-local cmp_status, cmp = pcall(require, "cmp")
-if not cmp_status then return end
-local cmp_action = require("lsp-zero").cmp_action()
+local status, cmp = pcall(require, "cmp")
+if (not status) then return end
+local status2, luasnip = pcall(require, "luasnip")
+if (not status2) then return end
 
-cmp.setup({
-  preselect = 'item',
-  completion = {
-    completeopt = 'menu,menuone,noinsert'
-  },
-  sources = {
-    { name = "nvim_lsp", priority = 1000, keyword_length = 1 }, -- Muestra sugerencias basadas en la respuesta de un servidor LSP
-    { name = "luasnip",  priority = 750,  keyword_length = 2 }, -- Muestra los snippets cargados. Si elegimos un snippet lo expande
-    { name = "buffer",   priority = 500,  keyword_length = 3 }, -- Sugiere palabras que se encuentra en el archivo actual
-    { name = "path",     priority = 250 },                      -- Autocompleta rutas de archivos
-  },
-  mapping = {
-    ['<Tab>'] = cmp_action.luasnip_supertab(),
-    ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
-  },
-  -- window = {
-  --   completion = cmp.config.window.bordered(),
-  --   documentation = cmp.config.window.bordered(),
-  -- },
-  formatting = {
-    fields = { 'menu', 'abbr', 'kind' },
-    format = function(entry, item)
-      local menu_icon = {
-        nvim_lsp = 'λ',
-        luasnip = '⋗',
-        buffer = 'Ω',
-        path = '🖫',
-        nvim_lua = 'Π',
-      }
-      item.menu = menu_icon[entry.source.name]
-      return item
+require('luasnip.loaders.from_vscode').lazy_load()
+
+local cmp_action = require("lsp-zero").cmp_action()
+cmp.setup {
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      luasnip.lsp_expand(args.body) -- For `luasnip` users.
     end,
   },
-})
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = false }),
+    ['<Tab>'] = cmp_action.luasnip_supertab(),
+    ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' }, -- For luasnip users.
+  }, {
+    { name = 'buffer' },
+  })
+}
