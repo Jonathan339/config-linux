19833b609e7c641e55f82b49639e2d730ecff55f
Jonathan David Dominguez <dominguez.jonathan.david@gmail.com>
2023-09-14 17:09:25 -0300

    feat(telescope): Elimine el mapeo de teclado

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -3,15 +3,22 @@ local devicons_status, devicons = pcall(require, "nvim-web-devicons")
 if not devicons_status then return end
 
 require("telescope").setup {
-   defaults = {
+  pickers = {
+    find_files = {
+      theme = "dropdown",
+    },
+    diagnostics = {
+      theme = "dropdown",
+    },
+  },
+  defaults = {
     file_ignore_patterns = { "node_modules" },
     dynamic_preview_title = true,
     path_display = { 'smart' },
   },
-
   extensions = {
     file_browser = {
-      theme = "ivy",
+      theme = "dropdown",
       cwd_to_path = false,
       grouped = false,
       files = true,
@@ -26,28 +33,10 @@ require("telescope").setup {
       hijack_netrw = false,
       use_fd = true,
       git_status = true,
-      -- mappings = {
-      --   ["i"] = {
-      --     ["<C-c>"] = actions.create,
-      --     ["<C-R>"] = actions.create_from_prompt,
-      --     ["<C-r>"] = actions.rename,
-      --     ["<C-m>"] = actions.move,
-      --     ["<C-y>"] = actions.copy,
-      --     ["<C-d>"] = actions.remove,
-      --     -- Otros mapeos personalizados para el modo inserción
-      --   },
-      --   ["n"] = {
-      --     ["c"] = actions.create,
-      --     ["r"] = actions.rename,
-      --     ["m"] = actions.move,
-      --     ["y"] = actions.copy,
-      --     ["d"] = actions.remove,
-      --     -- Otros mapeos personalizados para el modo normal
-      --   },
-      -- },
+
     },
   },
 }
 
--- Cargar la extensión después de configurar
-require("telescope").load_extension "file_browser"
+-- Cargar la extensión después de con:figurar
+require("telescope").load_extension("file_browser")
