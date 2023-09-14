3620168cf827b4ba672f1a2883fa459caaa430a0
Jonathan David Dominguez <dominguez.jonathan.david@gmail.com>
2023-09-14 17:11:39 -0300

    feat(null_ls): Cambie la configuracion de null_ls

--- nvim/after/plugin/null_ls.lua
+++ nvim/after/plugin/null_ls.lua
@@ -1,17 +1,27 @@
-local null_ls_status, null_ls = pcall(require, "null_ls")
-if not null_ls_status then
-  return
-end
+local status, null_ls = pcall(require, "null-ls")
+if not status then return end
 
-null_ls.setup {
-  sources = {
-    null_ls.builtins.diagnostics.eslint_d,
-    null_ls.builtins.formatting.eslint,
-    null_ls.builtins.formatting.prettierd,
-    null_ls.builtins.formatting.stylua,
-    null_ls.builtins.diagnostics.eslint_d.with({
-      diagnostics_format = '[eslint] #{m}\n(#{c})'
-    }),
-    null_ls.builtins.diagnostics.fish
-  },
+local formatting = null_ls.builtins.formatting
+local code_actions = null_ls.builtins.code_actions
+local diagnostics = null_ls.builtins.diagnostics
+
+local sources = {
+  --[[ formatting ]]
+  formatting.eslint_d,
+  formatting.autopep8,
+  formatting.stylua,
+  formatting.stylelint,
+  formatting.prettier,
+
+  --[[ code actions ]]
+  code_actions.eslint_d,
+  code_actions.refactoring,
+
+  --[[ diagnostics ]]
+  diagnostics.eslint_d,
+  -- diagnostics.flake8,
 }
+
+null_ls.setup({
+  sources = sources,
+})
