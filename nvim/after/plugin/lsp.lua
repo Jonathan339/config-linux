9829a89bd479d2dbbd47a7283913dfd95b23a2e8
Jonathan David Dominguez <dominguez.jonathan.david@gmail.com>
2023-09-14 17:06:55 -0300

    feat(nvim): Agregue el autoformateo con lsp

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,8 +1,26 @@
 local lsp_status, lsp = pcall(require, "lsp-zero")
 if not lsp_status then return end
+lsp.preset('recommended')
+
+local protocol = require('vim.lsp.protocol')
+
+local augroup_format = vim.api.nvim_create_augroup("Format", { clear = true })
+local enable_format_on_save = function(_, bufnr)
+  vim.api.nvim_clear_autocmds({ group = augroup_format, buffer = bufnr })
+  vim.api.nvim_create_autocmd("BufWritePre", {
+    group = augroup_format,
+    buffer = bufnr,
+    callback = function()
+      vim.lsp.buf.format({ bufnr = bufnr })
+    end,
+  })
+end
 
 lsp.on_attach(function(client, bufnr)
   lsp.default_keymaps({ buffer = bufnr })
+  lsp.dir.find_all({ 'package.json', 'vetur.config.js', 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml',
+    'node_modules', 'npm-shrinkwrap.json', '.git' })
+  enable_format_on_save(client, bufnr)
 end)
 -- Configura el formateo automático al guardar para ciertos servidores LSP y lenguajes.
 lsp.format_on_save({
@@ -30,19 +48,8 @@ lsp.set_server_config({
 
 -- Instala servidores LSP y complementos.
 lsp.ensure_installed({
-  "eslint_lsp",
-  "rust_analyzer",
-  "sumneko_lua",
-  "lua_ls",
-  "prettier",
-  "tsserver",
-  "eslint",
-  "bashls",
-  "vimls",
-  "lua_format",
-  "jsonls",
-})
 
+})
 -- Configura los íconos para los errores, advertencias, sugerencias e información del LSP.
 lsp.set_sign_icons({
   error = "✘",
@@ -50,14 +57,18 @@ lsp.set_sign_icons({
   hint = "⚑",
   info = "»",
 })
--- Configura la función de autocompletado del LSP.
-lsp.omnifunc.setup({
-  autocomplete = true,
-  use_fallback = true,
-  update_on_delete = true,
-  trigger = "<C-Space>",
-})
 
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+lsp.setup({
+  on_attach = lsp.on_attach,
+  enabled = true,
+})
 
-lsp.setup()
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    underline = true,
+    update_in_insert = false,
+    virtual_text = { spacing = 4, prefix = "\u{ea71}" },
+    severity_sort = true,
+  }
+)
