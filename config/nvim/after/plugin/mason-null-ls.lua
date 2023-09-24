6f5bc4d21b60808321485d89c09bef9c47cb154e
Jonathan David Dominguez <dominguez.jonathan.david@gmail.com>
2023-09-24 20:15:12 -0300

    feat(mason_null): Elimine comentarios

--- config/nvim/after/plugin/mason-null-ls.lua
+++ config/nvim/after/plugin/mason-null-ls.lua
@@ -8,11 +8,4 @@ end
 mason_null.setup({
   ensure_installed = nil,
   automatic_installation = true,
-  -- handlers = {
-  -- Here you can add functions to register sources.
-  -- See https://github.com/jay-babu/mason-null-ls.nvim#handlers-usage
-  --
-  -- If left empty, mason-null-ls will  use a "default handler"
-  -- to register all sources
-  -- },
 })
