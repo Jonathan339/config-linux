f4304241a579987cc94d2b73dc8cea574a4ab724
Jonathan David Dominguez <dominguez.jonathan.david@gmail.com>
2023-09-24 20:13:35 -0300

    feat(treesitter.lua): Agregue mas servidores

--- config/nvim/after/plugin/treesitter.lua
+++ config/nvim/after/plugin/treesitter.lua
@@ -3,27 +3,32 @@ if not treesitter_status then return end
 
 treesitter.setup({
   -- A list of parser names, or "all"
-  ensure_installed = {
-    "bash",
-    "python",
-    "lua",
-    "javascript",
-    "typescript",
-    "tsx",
-    "css",
-    "json",
-    "html",
-  },
-  -- ensure_installed = "all",
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
-
-  highlight = {
-    -- `false` will disable the whole extension
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
+   ensure_installed = {
+                    "javascript",
+                    "typescript",
+                    "c",
+                    "lua",
+                    "vim",
+                    "vimdoc",
+                    "query",
+                    "elixir",
+                    "erlang",
+                    "heex",
+                    "eex",
+                    "java",
+                    "kotlin",
+                    "jq",
+                    "dockerfile",
+                    "json",
+                    "html",
+                    "terraform",
+                    "go",
+                    "tsx",
+                    "bash",
+                    "ruby",
+                },
+                sync_install = false,
+                highlight = { enable = true },
+                indent = { enable = true },
 })
+     
