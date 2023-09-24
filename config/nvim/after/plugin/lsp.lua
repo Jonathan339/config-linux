1d0af756cd4490f41e39937d92af10d2df90dfa9
Jonathan David Dominguez <dominguez.jonathan.david@gmail.com>
2023-09-24 20:12:12 -0300

    feat(lsp.lua): Cambie el la configuracion

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -2,73 +2,99 @@ local lsp_status, lsp = pcall(require, "lsp-zero")
 if not lsp_status then return end
 lsp.preset('recommended')
 
-local protocol = require('vim.lsp.protocol')
+lsp.ensure_installed({
+  'tsserver',
+  'eslint',
+  'rust_analyzer',
+  'kotlin_language_server',
+  'jdtls',
+  'lua_ls',
+  'jsonls',
+  'html',
+  'elixirls',
+  'tailwindcss',
+  'tflint',
+  'pylsp',
+  'dockerls',
+  'bashls',
+  'marksman',
+  'solargraph',
+})
 
-local augroup_format = vim.api.nvim_create_augroup("Format", { clear = true })
-local enable_format_on_save = function(_, bufnr)
-  vim.api.nvim_clear_autocmds({ group = augroup_format, buffer = bufnr })
-  vim.api.nvim_create_autocmd("BufWritePre", {
-    group = augroup_format,
-    buffer = bufnr,
-    callback = function()
-      vim.lsp.buf.format({ bufnr = bufnr })
-    end,
-  })
-end
+local cmp = require("cmp")
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+local cmp_mappings = lsp.defaults.cmp_mappings({
+  ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+  ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+  ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+  ["<CR>"] = cmp.mapping.confirm({ select = true }),
+  ['<C-Space>'] = cmp.mapping.complete(),
+})
+
+lsp.setup_nvim_cmp({
+  mapping = cmp_mappings
+})
 
 lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({ buffer = bufnr })
-  lsp.dir.find_all({ 'package.json', 'vetur.config.js', 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml',
-    'node_modules', 'npm-shrinkwrap.json', '.git' })
-  enable_format_on_save(client, bufnr)
+  local opts = { buffer = bufnr, remap = false }
+
+  vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts, { desc = "LSP Goto Reference" })
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts, { desc = "LSP Goto Definition" })
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts, { desc = "LSP Hover" })
+  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts,
+    { desc = "LSP Workspace Symbol" })
+  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.setloclist() end, opts, { desc = "LSP Show Diagnostics" })
+  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts, { desc = "Next Diagnostic" })
+  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts, { desc = "Previous Diagnostic" })
+  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts, { desc = "LSP Code Action" })
+  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts, { desc = "LSP References" })
+  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts, { desc = "LSP Rename" })
+  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts, { desc = "LSP Signature Help" })
 end)
--- Configura el formateo automático al guardar para ciertos servidores LSP y lenguajes.
-lsp.format_on_save({
-  enable = true,
-  ormat_opts = {
-    async = true,
-    timeout_ms = 10000,
-  },
-  servers = {
-    ["null-ls"] = { "javascript", "typescript", "lua" },
-  },
-})
-
-lsp.set_server_config({
-  single_file_support = false,
-  capabilities = {
-    textDocument = {
-      foldingRange = {
-        dynamicRegistration = false,
-        lineFoldingOnly = true
-      }
-    }
-  }
-})
-
--- Instala servidores LSP y complementos.
-lsp.ensure_installed({
-
-})
--- Configura los íconos para los errores, advertencias, sugerencias e información del LSP.
-lsp.set_sign_icons({
-  error = "✘",
-  warn = "▲",
-  hint = "⚑",
-  info = "»",
-})
 
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
-lsp.setup({
-  on_attach = lsp.on_attach,
-  enabled = true,
+
+lsp.setup()
+
+local cmp_action = require('lsp-zero').cmp_action()
+
+require('luasnip.loaders.from_vscode').lazy_load()
+
+-- `/` cmdline setup.
+cmp.setup.cmdline('/', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
 })
 
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-    underline = true,
-    update_in_insert = false,
-    virtual_text = { spacing = 4, prefix = "\u{ea71}" },
-    severity_sort = true,
-  }
-)
+-- `:` cmdline setup.
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    {
+      name = 'cmdline',
+      option = {
+        ignore_cmds = { 'Man', '!' }
+      }
+    }
+  })
+})
+
+cmp.setup({
+  sources = {
+    { name = 'codeium' },
+    { name = 'nvim_lsp' },
+    { name = 'luasnip', keyword_length = 2 },
+    { name = 'buffer',  keyword_length = 3 },
+    { name = 'path' },
+  },
+  mapping = {
+    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+    ['<Tab>'] = cmp_action.luasnip_supertab(),
+    ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+  },
+})
