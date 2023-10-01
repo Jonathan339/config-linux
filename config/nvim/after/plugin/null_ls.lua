5412c13b0844e824715b0b9f0a9cd07f52e6632a
Jonathan David Dominguez <dominguez.jonathan.david@gmail.com>
2023-10-01 08:57:57 -0300

    feat(null_ls): Agregue servidores

--- config/nvim/after/plugin/null_ls.lua
+++ config/nvim/after/plugin/null_ls.lua
@@ -1,26 +1,36 @@
 local status, null_ls = pcall(require, "null-ls")
 if not status then return end
 
-local formatting = null_ls.builtins.formatting
-local code_actions = null_ls.builtins.code_actions
-local diagnostics = null_ls.builtins.diagnostics
-
 local sources = {
-  --[[ formatting ]]
-  formatting.eslint_d,
-  formatting.autopep8,
-  formatting.stylua,
-  formatting.stylelint,
-  formatting.prettier,
+  -- Formateo
+  null_ls.builtins.formatting.stylua,
+  null_ls.builtins.formatting.prettier_standard,
+  null_ls.builtins.formatting.standardjs,  -- Formateo con standardjs
 
-  --[[ code actions ]]
-  code_actions.eslint_d,
-  code_actions.refactoring,
+  -- Diagnósticos
+  null_ls.builtins.diagnostics.standardjs,  -- Diagnóstico con standardjs
+  null_ls.builtins.diagnostics.jsonlint,
+  null_ls.builtins.diagnostics.ktlint,
+  null_ls.builtins.diagnostics.markdownlint,
+  null_ls.builtins.diagnostics.protoc_gen_lint,
+  null_ls.builtins.diagnostics.fish,
+  --null_ls.builtins.diagnostics.selene,
 
-  --[[ diagnostics ]]
-  diagnostics.eslint_d,
-  -- diagnostics.flake8,
+  -- Completado de ortografía
+  null_ls.builtins.completion.spell,
+  null_ls.builtins.completion.tags,
+
+  -- Acciones de código
+  null_ls.builtins.code_actions.eslint,
+  null_ls.builtins.code_actions.refactoring,
+  null_ls.builtins.code_actions.statix,
+  --null_ls.builtins.code_actions.ts_node_action,
+    -- Mantener para otras acciones de código
+  -- hover
+  null_ls.builtins.hover.dictionary 
 }
+
+-- LSP setup y otras configuraciones aquí...
 
 null_ls.setup({
   sources = sources,
