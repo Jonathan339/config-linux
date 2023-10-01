910b8f6412e9517e275681af982e3dcd43e3f6c3
Jonathan D. Dominguez <30633652+Jonathan339@users.noreply.github.com>
2023-10-01 10:50:04 -0300

    Update null_ls.lua

--- config/nvim/after/plugin/null_ls.lua
+++ config/nvim/after/plugin/null_ls.lua
@@ -1,33 +1,38 @@
 local status, null_ls = pcall(require, "null-ls")
 if not status then return end
 
+local formatting = null_ls.builtins.formatting
+local code_actions = null_ls.builtins.code_actions
+local diagnostics = null_ls.builtins.diagnostics
+local completion = null_ls.builtins.completion
+local hover = null_ls.builtins.hover
 local sources = {
   -- Formateo
-  null_ls.builtins.formatting.stylua,
-  null_ls.builtins.formatting.prettier_standard,
-  null_ls.builtins.formatting.standardjs,  -- Formateo con standardjs
+  formatting.stylua,
+  formatting.prettier_standard,
+  formatting.standardjs,  -- Formateo con standardjs
 
   -- Diagnósticos
-  null_ls.builtins.diagnostics.standardjs,  -- Diagnóstico con standardjs
-  null_ls.builtins.diagnostics.jsonlint,
-  null_ls.builtins.diagnostics.ktlint,
-  null_ls.builtins.diagnostics.markdownlint,
-  null_ls.builtins.diagnostics.protoc_gen_lint,
-  null_ls.builtins.diagnostics.fish,
-  --null_ls.builtins.diagnostics.selene,
+  diagnostics.standardjs,  -- Diagnóstico con standardjs
+  diagnostics.jsonlint,
+  diagnostics.ktlint,
+  diagnostics.markdownlint,
+  diagnostics.protoc_gen_lint,
+  diagnostics.fish,
+  --diagnostics.selene,
 
   -- Completado de ortografía
-  null_ls.builtins.completion.spell,
-  null_ls.builtins.completion.tags,
+  completion.spell,
+  completion.tags,
 
   -- Acciones de código
-  null_ls.builtins.code_actions.eslint,
-  null_ls.builtins.code_actions.refactoring,
-  null_ls.builtins.code_actions.statix,
-  --null_ls.builtins.code_actions.ts_node_action,
-    -- Mantener para otras acciones de código
+  code_actions.eslint,
+  code_actions.refactoring,
+  code_actions.statix,
+  --code_actions.ts_node_action,
+  -- Mantener para otras acciones de código
   -- hover
-  null_ls.builtins.hover.dictionary 
+  hover.dictionary
 }
 
 -- LSP setup y otras configuraciones aquí...
