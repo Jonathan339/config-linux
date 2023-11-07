3e9d88d658865e67490514150219687a108a5181
Jonathan David Dominguez <dominguez.jonathan.david@gmail.com>
2023-11-06 21:09:04 -0300

    quite ui de lazy.lua

--- config/nvim/lua/core/lazy.lua
+++ config/nvim/lua/core/lazy.lua
@@ -24,78 +24,30 @@ require("lazy").setup({{ import = "plugins" },
 		-- try to load one of these colorschemes when starting an installation during startup
 		colorscheme = { 'onedark' },
 	  },
-	  ui = {
-		size = { width = 0.8, height = 0.8 },
-		border = 'none',
-		icons = {
-		  loaded = '●',
-		  not_loaded = '○',
-		  cmd = ' ',
-		  config = '',
-		  event = '',
-		  ft = ' ',
-		  init = ' ',
-		  keys = ' ',
-		  plugin = ' ',
-		  runtime = ' ',
-		  source = ' ',
-		  start = '',
-		  task = '✔ ',
-		  lazy = '鈴 ',
-		  list = {
-			'●',
-			'➜',
-			'★',
-			'‒',
-		  },
+	  rtp = {
+		reset = true, -- reset the runtime path to $VIMRUNTIME and your config directory
+		---@type string[]
+		paths = {}, -- add any custom paths here that you want to incluce in the rtp
+		---@type string[] list any plugins you want to disable here
+		disabled_plugins = {
+		  "matchit",
+		  "netrw",
+		  "netrwPlugin",
+		  "netrwSettings",
+		  "netrwFileHandlers",
+		  "gzip",
+		  "zip",
+		  "zipPlugin",
+		  "tar",
+		  "tarPlugin",
+		  "getscript",
+		  "getscriptPlugin",
+		  "vimball",
+		  "vimballPlugin",
+		  "2html_plugin",
+		  "logipat",
+		  "rrhelper",
+		  "matchparen"
 		},
-	  },
-	  diff = { cmd = 'diffview.nvim' },
-	  checker = {
-		-- automatically check for plugin updates
-		enabled = false,
-		concurrency = nil, ---@type number? set to 1 to check for updates very slowly
-		notify = true, -- get a notification when new updates are found
-		frequency = 3600, -- check for updates every hour
-	  },
-	  change_detection = {
-		enabled = true,
-		notify = true, -- get a notification when changes are found
-	  },
-	  performance = {
-		cache = {
-		  enabled = true,
-		  path = vim.fn.stdpath('cache') .. '/lazy/cache',
-		  disable_events = { 'VimEnter', 'BufReadPre' },
-		  ttl = 3600 * 24 * 5, -- keep unused modules for up to 5 days
-		},
-		reset_packpath = true, -- reset the package path to improve startup time
-		rtp = {
-		  reset = true, -- reset the runtime path to $VIMRUNTIME and your config directory
-		  ---@type string[]
-		  paths = {}, -- add any custom paths here that you want to incluce in the rtp
-		  ---@type string[] list any plugins you want to disable here
-		  disabled_plugins = {
-			"matchit",
-			"netrw",
-			"netrwPlugin",
-			"netrwSettings",
-			"netrwFileHandlers",
-			"gzip",
-			"zip",
-			"zipPlugin",
-			"tar",
-			"tarPlugin",
-			"getscript",
-			"getscriptPlugin",
-			"vimball",
-			"vimballPlugin",
-			-- "2html_plugin",
-			"logipat",
-			"rrhelper",
-			"matchparen"
-		  },
-		},
-	  },
   })
 
