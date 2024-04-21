cec146363284f8076aa4ff28b47a7091d7d99fdd
Jonathan David Dominguez <dominguez.jonathan.david@gmail.com>
2024-04-21 16:19:11 -0300

    edite conform.nvim

--- config/nvim/lua/plugins/lsp/conform.lua
+++ config/nvim/lua/plugins/lsp/conform.lua
@@ -1,45 +1,45 @@
 return {
-    'stevearc/conform.nvim',
-   -- event = { 'BufWritePre' },
-    cmd = { 'ConformInfo' },
-    keys = {
-      {
-        -- Customize or remove this keymap to your liking
-        '<leader>.',
-        function()
-          require('conform').format({ async = true, lsp_fallback = true })
-        end,
-        mode = '',
-        desc = 'Format buffer',
+  'stevearc/conform.nvim',
+  event = { 'BufWritePre' },
+  cmd = { 'ConformInfo' },
+  keys = {
+    {
+      -- Customize or remove this keymap to your liking
+      '<leader>.',
+      function()
+        require('conform').format({ async = true, lsp_fallback = true })
+      end,
+      mode = '',
+      desc = 'Format buffer',
+    },
+  },
+  -- Everything in opts will be passed to setup()
+  opts = {
+    -- Define your formatters
+    formatters_by_ft = {
+      lua = { "stylua" },
+      -- Conform will run multiple formatters sequentially
+      python = { "isort", "black" },
+      -- Use a sub-list to run only the first available formatter
+      javascript = { { "standardjs" } },
+    },
+    -- Set up format-on-save
+    format_on_save = {
+      timeout_ms = 4000,
+      async = true,        -- not recommended to change
+      quiet = false,       -- not recommended to change
+      lsp_fallback = true, -- not recommended to change
+    },
+    -- Customize formatters
+    formatters = {
+      injected = { options = { ignore_errors = true } },
+      shfmt = {
+        prepend_args = { '-i', '2' },
       },
     },
-    -- Everything in opts will be passed to setup()
-    opts = {
-      -- Define your formatters
-      formatters_by_ft = {
-        lua = { "stylua" },
-        -- Conform will run multiple formatters sequentially
-        python = { "isort", "black" },
-        -- Use a sub-list to run only the first available formatter
-        javascript = { { "prettierd", "prettier" } },
-      },
-      -- Set up format-on-save
-      format_on_save = {
-        timeout_ms = 3000,
-        async = true,       -- not recommended to change
-        quiet = false,       -- not recommended to change
-        lsp_fallback = true, -- not recommended to change
-      },
-      -- Customize formatters
-      formatters = {
-        injected = { options = { ignore_errors = true } },
-        shfmt = {
-          prepend_args = { '-i', '2' },
-        },
-      },
-    },
-    init = function()
-      -- If you want the formatexpr, here is the place to set it
-      vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-    end,
-  }
+  },
+  init = function()
+    -- If you want the formatexpr, here is the place to set it
+    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+  end,
+}
