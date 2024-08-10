fb9f90056b0d3f44f3bc0cd414b7e52aa2f7f978
Jonathan David Dominguez <dominguez.jonathan.david@gmail.com>
2024-08-10 11:55:56 -0300

    fix: se cambio la version de telescope para solucionar el error de deprecacion

--- config/nvim/lua/plugins/ui/telescope.lua
+++ config/nvim/lua/plugins/ui/telescope.lua
@@ -1,24 +1,24 @@
 local M = {
-	"nvim-telescope/telescope.nvim",
-	event = "VeryLazy",
-	tag = "0.1.3",
-	-- or                              , branch = '0.1.x',
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-telescope/telescope-live-grep-args.nvim",
-		"nvim-telescope/telescope-file-browser.nvim",
-	},
+  'nvim-telescope/telescope.nvim',
+  event = 'VeryLazy',
+  tag = '0.1.8',
+  -- or                              , branch = '0.1.x',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope-live-grep-args.nvim',
+    'nvim-telescope/telescope-file-browser.nvim',
+  },
 }
 
 M.config = function()
-	local telescope = require("telescope")
+  local telescope = require('telescope')
 
-	-- Configurar las opciones predeterminadas de Telescope
-	telescope.setup({ defaults = { file_ignore_patterns = { "node_modules" } } })
+  -- Configurar las opciones predeterminadas de Telescope
+  telescope.setup({ defaults = { file_ignore_patterns = { 'node_modules' } } })
 
-	-- Cargar extensiones adicionales de Telescope
-	telescope.load_extension("file_browser")
-	telescope.load_extension("live_grep_args")
+  -- Cargar extensiones adicionales de Telescope
+  telescope.load_extension('file_browser')
+  telescope.load_extension('live_grep_args')
 end
 
 return M
