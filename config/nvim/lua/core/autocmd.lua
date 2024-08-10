cedaf1c8dcfd32f32e9bbdae6c0510c8b0eebd3e
Jonathan David Dominguez <dominguez.jonathan.david@gmail.com>
2024-08-10 18:02:45 -0300

    refactor: elimine cosas de autocmd.lua

--- config/nvim/lua/core/autocmd.lua
+++ config/nvim/lua/core/autocmd.lua
@@ -1,11 +1,7 @@
 local agroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
-local usercmd = vim.api.nvim_create_user_command
-
--- This file is automatically loaded by lazyvim.config.init.
-
-local function augroup(name)
-  return vim.api.nvim_create_augroup('lazyvim_' .. name, { clear = true })
+local augroup = function(name)
+  return agroup('lazyvim_' .. name, { clear = true })
 end
 
 -- Check if we need to reload the file when it changed
@@ -18,56 +14,39 @@ autocmd({ 'FocusGained', 'TermClose', 'TermLeave' }, {
   end,
 })
 
-agroup('__formatter__', { clear = true })
+-- Auto format on save
 autocmd('BufWritePost', {
-  group = '__formatter__',
+  group = augroup('__formatter__'),
   command = ':FormatWrite',
 })
 
--- local function augroup(name)
---   return vim.api.nvim_create_augroup('lazyvim_' .. name, { clear = true })
--- end
--- Turn off paste mode when leaving insert
--- https://github.com/tpope/vim-sensible/blob/master/plugin/sensible.vim
+-- Set custom highlights for completion menu
 autocmd('TextChanged', {
-  pattern = '*',
   callback = function()
     vim.api.nvim_set_hl(0, 'CmpItemAbbrDeprecated', { bg = 'NONE', strikethrough = true, fg = '#808080' })
-    -- blue
     vim.api.nvim_set_hl(0, 'CmpItemAbbrMatch', { bg = 'NONE', fg = '#569CD6' })
     vim.api.nvim_set_hl(0, 'CmpItemAbbrMatchFuzzy', { link = 'CmpItemAbbrMatch' })
-    -- light blue
     vim.api.nvim_set_hl(0, 'CmpItemKindVariable', { bg = 'NONE', fg = '#9CDCFE' })
     vim.api.nvim_set_hl(0, 'CmpItemKindInterface', { link = 'CmpItemKindVariable' })
     vim.api.nvim_set_hl(0, 'CmpItemKindText', { link = 'CmpItemKindVariable' })
-    -- pink
     vim.api.nvim_set_hl(0, 'CmpItemKindFunction', { bg = 'NONE', fg = '#C586C0' })
     vim.api.nvim_set_hl(0, 'CmpItemKindMethod', { link = 'CmpItemKindFunction' })
-    -- front
     vim.api.nvim_set_hl(0, 'CmpItemKindKeyword', { bg = 'NONE', fg = '#D4D4D4' })
     vim.api.nvim_set_hl(0, 'CmpItemKindProperty', { link = 'CmpItemKindKeyword' })
     vim.api.nvim_set_hl(0, 'CmpItemKindUnit', { link = 'CmpItemKindKeyword' })
   end,
 })
 
--- Check if we need to reload the file when it changed
-autocmd({ 'FocusGained', 'TermClose', 'TermLeave' }, {
-  group = augroup('checktime'),
-  callback = function()
-    if vim.o.buftype ~= 'nofile' then
-      vim.cmd('checktime')
-    end
-  end,
-})
-
+-- Remove trailing spaces before saving
 autocmd('BufWritePre', {
+  group = augroup('trim_trailing_spaces'),
   callback = function()
     vim.cmd([[%s/\s\+$//e]])
   end,
 })
--- resize splits if window got resized
 
-autocmd({ 'VimResized' }, {
+-- Resize splits if window got resized
+autocmd('VimResized', {
   group = augroup('resize_splits'),
   callback = function()
     local current_tab = vim.fn.tabpagenr()
@@ -84,43 +63,18 @@ autocmd('TextYankPost', {
   end,
 })
 
--- close some filetypes with <q>
-autocmd('FileType', {
-  group = augroup('close_with_q'),
-  pattern = {
-    'PlenaryTestPopup',
-    'help',
-    'lspinfo',
-    'notify',
-    'qf',
-    'query',
-    'spectre_panel',
-    'startuptime',
-    'tsplayground',
-    'neotest-output',
-    'checkhealth',
-    'neotest-summary',
-    'neotest-output-panel',
-  },
-  callback = function(event)
-    vim.bo[event.buf].buflisted = false
-    vim.keymap.set('n', 'q', '<cmd>close<cr>', { buffer = event.buf, silent = true })
-  end,
-})
-
--- Auto create dir when saving a file, in case some intermediate directory does not exist
-autocmd({ 'BufWritePre' }, {
+-- Auto create dir when saving a file
+autocmd('BufWritePre', {
   group = augroup('auto_create_dir'),
   callback = function(event)
-    if event.match:match('^%w%w+:[\\/][\\/]') then
-      return
+    if not event.match:match('^%w%w+:[\\/][\\/]') then
+      local file = vim.uv.fs_realpath(event.match) or event.match
+      vim.fn.mkdir(vim.fn.fnamemodify(file, ':p:h'), 'p')
     end
-    local file = vim.uv.fs_realpath(event.match) or event.match
-    vim.fn.mkdir(vim.fn.fnamemodify(file, ':p:h'), 'p')
   end,
 })
 
--- close some filetypes with <q>
+-- Close specific filetypes with <q>
 autocmd('FileType', {
   group = augroup('close_with_q'),
   pattern = {
@@ -144,17 +98,26 @@ autocmd('FileType', {
   end,
 })
 
--- autocmd('BufWritePre', {
---   pattern = '*',
---   callback = function(args)
---     require('conform').format({ bufnr = args.buf })
---   end,
--- })
-
+-- Remove luasnip snippets when leaving insert mode
 autocmd('InsertLeave', {
   callback = function()
-    if require('luasnip').session.current_nodes[vim.api.nvim_get_current_buf()] and not require('luasnip').session.jump_active then
-      require('luasnip').unlink_current()
+    local luasnip = require('luasnip')
+    if luasnip.session.current_nodes[vim.api.nvim_get_current_buf()] and not luasnip.session.jump_active then
+      luasnip.unlink_current()
+    end
+  end,
+})
+
+autocmd('BufReadPre', {
+  group = augroup('large_file_optimizations'),
+  callback = function()
+    local max_size = 100 * 1024 -- 100 KB
+    local file_size = vim.fn.getfsize(vim.fn.expand('<afile>'))
+    if file_size > max_size then
+      vim.cmd('syntax off')
+      vim.cmd('setlocal noswapfile bufhidden=unload')
+      vim.cmd('setlocal noundofile')
+      vim.cmd('setlocal nofoldenable')
     end
   end,
 })
