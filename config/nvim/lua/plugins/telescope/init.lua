3dd651e92cf39f9d7ef18ecfb528443e09ace362
Jonathan David Dominguez <dominguez.jonathan.david@gmail.com>
2024-09-01 16:41:05 -0300

    chore: cambie la configuracion de telescope

--- config/nvim/lua/plugins/telescope/init.lua
+++ config/nvim/lua/plugins/telescope/init.lua
@@ -2,7 +2,6 @@ local M = {
   'nvim-telescope/telescope.nvim',
   event = 'VeryLazy',
   tag = '0.1.8',
-  -- or                              , branch = '0.1.x',
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-telescope/telescope-live-grep-args.nvim',
@@ -12,9 +11,48 @@ local M = {
 
 M.config = function()
   local telescope = require('telescope')
+  local actions = require('telescope.actions')
+  local action_state = require('telescope.actions.state')
 
   -- Configurar las opciones predeterminadas de Telescope
-  telescope.setup({ defaults = { file_ignore_patterns = { 'node_modules' } } })
+  telescope.setup({
+    defaults = {
+      file_ignore_patterns = { 'node_modules' },
+      prompt_prefix = '❯ ',
+      selection_caret = '❯ ',
+      winblend = 0,
+      layout_strategy = 'horizontal',
+      layout_config = {
+        width = 0.95,
+        height = 0.85,
+        prompt_position = 'bottom',
+        horizontal = {
+          preview_width = function(_, cols, _)
+            if cols > 200 then
+              return math.floor(cols * 0.5)
+            else
+              return math.floor(cols * 0.7)
+            end
+          end,
+        },
+      },
+      selection_strategy = 'reset',
+      sorting_strategy = 'descending',
+      scroll_strategy = 'cycle',
+      color_devicons = true,
+      mappings = {
+        i = {
+          ['<C-n>'] = 'move_selection_next',
+          ['<C-y>'] = function(prompt_bufnr)
+            local entry = action_state.get_selected_entry()
+            if entry then
+              action_state.get_current_picker(prompt_bufnr):reset_prompt(entry.ordinal)
+            end
+          end,
+        },
+      },
+    },
+  })
 
   -- Cargar extensiones adicionales de Telescope
   telescope.load_extension('file_browser')
